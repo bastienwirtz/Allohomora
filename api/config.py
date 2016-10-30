ad2ef8653d9306416de374f9fdec89d2df786e4e
bastienwirtz <bastien.wirtz@gmail.com>
2016-10-30 13:20:45 +0100

    Debug mode can be enable by configuration

--- api/config.py
+++ api/config.py
@@ -1,3 +1,6 @@
 # GPIO address
 OPEN_TIME = 5
 RELAY_IO = 17
+
+# App settings
+DEBUG = True
--- api/run.py
+++ api/run.py
@@ -1,5 +1,6 @@
 import sys
 import signal
+import config
 from allohomora import app, views
 
 
@@ -9,4 +10,4 @@ def shutdown(signum, frame):
 signal.signal(signal.SIGINT, shutdown)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=config.DEBUG)
